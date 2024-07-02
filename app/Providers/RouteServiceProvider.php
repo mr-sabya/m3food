a9cce41f2941f8eb2031eb4e35a41e9cf6d164de
sabyaroy-dev <sabya.citik@gmail.com>
2024-07-02 15:56:24 +0600

    add backend theme

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,6 +33,10 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::prefix('admin')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
         });
     }
 
